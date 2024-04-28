c2c2ad41cdb8ae61aef31627bce1f7ad025b8153
Anton Gatsko <89578801+T0IIIA@users.noreply.github.com>
2024-04-28 20:04:19 +0300

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,33 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.items = this.items.filter(item => item !== itemToRemove);
+  }
+
+};
+
+
+//=======================================================================//
+
+console.log("----------task-2----------");
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems());
+
+storage.addItem("Droid");
+console.log(storage.getItems());
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems());
+
+storage.removeItem("Scaner");
+console.log(storage.getItems());
---
+++ js/task-3.js
@@ -0,0 +1,37 @@
+class StringBuilder {
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value += str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+};
+
+//==============================================//
+
+console.log("----------task-3----------");
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue());
+
+builder.padStart("^");
+console.log(builder.getValue());
+
+builder.padEnd("^");
+console.log(builder.getValue());
+
+builder.padBoth("=");
+console.log(builder.getValue());
